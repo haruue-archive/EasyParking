565e6c7696e26a766a5c350bbd40010be95527d8
Haruue Icymoon <haruue@caoyue.com.cn>
2018-03-18 22:57:18 +0800

    feat(common): add animator to android:progress
    Signed-off-by: Haruue Icymoon <haruue@caoyue.com.cn>

--- common/src/main/java/moe/haruue/ep/common/databinding/Adapters.kt
+++ common/src/main/java/moe/haruue/ep/common/databinding/Adapters.kt
@@ -1,7 +1,9 @@
 package moe.haruue.ep.common.databinding
 
+import android.animation.ObjectAnimator
 import android.databinding.BindingAdapter
 import android.support.design.widget.TextInputLayout
+import android.widget.ProgressBar
 
 /**
  *
@@ -13,3 +15,17 @@ fun setError(view: TextInputLayout, error: String?) {
     view.error = error
 }
 
+@BindingAdapter("android:progress")
+fun setProgress(view: ProgressBar, progress: Int) {
+    val old = view.progress
+    val new = progress
+    if (old < new) {
+        ObjectAnimator.ofInt(view, "progress", old, new)
+                .apply {
+                    duration = 500
+                    setAutoCancel(true)
+                }.start()
+    } else {
+        view.progress = new
+    }
+}
