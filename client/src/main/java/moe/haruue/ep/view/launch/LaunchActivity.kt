48144091d916d0e8f40434478b7b1fcb66b1a213
Haruue Icymoon <haruue@caoyue.com.cn>
2018-05-14 20:05:26 +0800

    feat(client): require permission when launch
    Signed-off-by: Haruue Icymoon <haruue@caoyue.com.cn>

--- client/src/main/java/moe/haruue/ep/view/launch/LaunchActivity.kt
+++ client/src/main/java/moe/haruue/ep/view/launch/LaunchActivity.kt
@@ -1,10 +1,15 @@
 package moe.haruue.ep.view.launch
 
 import android.app.Activity
+import android.app.AlertDialog
 import android.arch.lifecycle.ViewModelProviders
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.databinding.DataBindingUtil
+import android.net.Uri
+import android.os.Build
 import android.os.Bundle
+import android.provider.Settings
 import android.support.v7.app.AppCompatActivity
 import moe.haruue.ep.R
 import moe.haruue.ep.databinding.ActivityLaunchBinding
@@ -21,12 +26,16 @@ class LaunchActivity : AppCompatActivity() {
 
     companion object {
         const val REQ_LOGIN = 0x1
+        const val REQ_PERMISSION_LOCATION = 0x2
+        const val PERMISSION_LOCATION = android.Manifest.permission.ACCESS_FINE_LOCATION
     }
+
+    lateinit var binding: ActivityLaunchBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        val binding = DataBindingUtil.setContentView<ActivityLaunchBinding>(this, R.layout.activity_launch).apply {
+        binding = DataBindingUtil.setContentView<ActivityLaunchBinding>(this, R.layout.activity_launch).apply {
             setLifecycleOwner(this@LaunchActivity::getLifecycle)
             m = ViewModelProviders.of(this@LaunchActivity)[LaunchViewModel::class.java].apply {
                 confirmed.observe(this@LaunchActivity::getLifecycle) {
@@ -40,10 +49,13 @@ class LaunchActivity : AppCompatActivity() {
                         startActivityForResult<LoginActivity>(REQ_LOGIN)
                     }
                 }
+                onCheckLocationPermission = this@LaunchActivity::onCheckLocationPermission
             }
         }
 
+
         binding.m!!.onCreate()
+
 
     }
 
@@ -57,6 +69,55 @@ class LaunchActivity : AppCompatActivity() {
                 finish()
             }
         }
+    }
+
+    lateinit var onCheckLocationPermissionCallback: (granted: Boolean) -> Unit
+
+    fun onCheckLocationPermission(callback: (granted: Boolean) -> Unit) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (checkSelfPermission(PERMISSION_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+                callback(true)
+            } else {
+                onCheckLocationPermissionCallback = callback
+                requestPermissions(arrayOf(PERMISSION_LOCATION), REQ_PERMISSION_LOCATION)
+            }
+        } else {
+            callback(true)
+        }
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when (requestCode) {
+            REQ_PERMISSION_LOCATION -> {
+                for ((i, p) in permissions.withIndex()) {
+                    if (p == PERMISSION_LOCATION) {
+                        if (grantResults[i] == PackageManager.PERMISSION_GRANTED) {
+                            onCheckLocationPermissionCallback(true)
+                        } else {
+                            onCheckLocationPermissionCallback(false)
+                            AlertDialog.Builder(this@LaunchActivity).apply {
+                                title = "授权遭拒"
+                                setMessage("没有位置权限，本应用将无法运行，请授予本应用位置权限！")
+                                setPositiveButton("去授予权限") { _, _ ->
+                                    val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS).apply {
+                                        data = Uri.fromParts("package", packageName, null)
+                                    }
+                                    startActivity(intent)
+                                }
+                                setNegativeButton("退出应用") { _, _ ->
+                                    finish()
+                                    System.exit(0)
+                                }
+
+                            }.create().show()
+                        }
+                    }
+                }
+            }
+        }
+
+
     }
 
     fun toMain() {
--- client/src/main/java/moe/haruue/ep/view/launch/LaunchViewModel.kt
+++ client/src/main/java/moe/haruue/ep/view/launch/LaunchViewModel.kt
@@ -20,6 +20,10 @@ class LaunchViewModel : ViewModel() {
     val needLogin = mutableLiveDataOf(false)
     val needRetry = mutableLiveDataOf(false)
 
+    var onCheckLocationPermission: (cb: (granted: Boolean) -> Unit) -> Unit = {
+        error("Please set LaunchViewModel#onCheckLocationPermission() and check permission in callback")
+    }
+
     fun onCreate() {
         checkLogin()
     }
@@ -33,37 +37,46 @@ class LaunchViewModel : ViewModel() {
         needLogin.value = false
         needRetry.value = false
         progress.postValue(10)
-        status.postValue("正在与服务器通讯...")
-        MainAPIService.with { it.accountRefresh() }
-                .observeOn(AndroidSchedulers.mainThread())
-                .apiSubscribe("LaunchViewModel#checkLogin") {
-                    onNext = {
-                        confirmed.postValue(true)
-                    }
-                    onComplete = {
-                        status.postValue("登录成功...")
-                    }
-                    onAPIError = {
-                        if (it.code == 401) {
-                            needLogin.postValue(true)
-                            status.postValue("正在跳转到登录界面...")
-                        } else {
-                            needRetry.postValue(true)
-                            status.postValue("身份认证出错: ${it.message}\n请点击下方按钮登录或重试。")
+        status.postValue("检查权限...")
+        onCheckLocationPermission {
+            if (it) {
+                status.postValue("正在与服务器通讯...")
+                MainAPIService.with { it.accountRefresh() }
+                        .observeOn(AndroidSchedulers.mainThread())
+                        .apiSubscribe("LaunchViewModel#checkLogin") {
+                            onNext = {
+                                confirmed.postValue(true)
+                            }
+                            onComplete = {
+                                status.postValue("登录成功...")
+                            }
+                            onAPIError = {
+                                if (it.code == 401) {
+                                    needLogin.postValue(true)
+                                    status.postValue("正在跳转到登录界面...")
+                                } else {
+                                    needRetry.postValue(true)
+                                    status.postValue("身份认证出错: ${it.message}\n请点击下方按钮登录或重试。")
+                                }
+                            }
+                            onNetworkError = {
+                                needRetry.postValue(true)
+                                status.postValue("网络错误: ${it.localizedMessage}\n请检查网络连接，然后点击下方按钮重试。")
+                            }
+                            onOtherError = {
+                                needRetry.postValue(true)
+                                status.postValue("未知错误: ${it.localizedMessage}\n请点击下方按钮登录或重试。")
+                            }
+                            onFinally = {
+                                progress.postValue(100)
+                            }
                         }
-                    }
-                    onNetworkError = {
-                        needRetry.postValue(true)
-                        status.postValue("网络错误: ${it.localizedMessage}\n请检查网络连接，然后点击下方按钮重试。")
-                    }
-                    onOtherError = {
-                        needRetry.postValue(true)
-                        status.postValue("未知错误: ${it.localizedMessage}\n请点击下方按钮登录或重试。")
-                    }
-                    onFinally = {
-                        progress.postValue(100)
-                    }
-                }
+            } else {
+                status.postValue("授权遭拒：没有位置权限本应用将无法运行！\n请授予权限，然后点击下方按钮重试。")
+                progress.postValue(100)
+                needRetry.postValue(true)
+            }
+        }
     }
 
 
